2495fb60ce840d07f110f811cbb432457e5ca6be
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-02 10:49:17 -0500

    completed steps 1 through 5 on Article.js

--- components/Article.js
+++ components/Article.js
@@ -86,6 +86,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'I Am Almost Finished with this Project',
+    data: 'Sep 2nd, 2020',
+    firstParagraph: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    
+    secondParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    thirdParagraph: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
   }
 ];
 
@@ -114,3 +123,50 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+function articleMaker(articleObj){
+  // creating div element and adding 'article' class to it
+  const article = document.createElement('div')
+  article.classList.add('article');
+  
+  // creating h2 element and adding it as child of article
+  const articleH2 = document.createElement('h2');
+  articleH2.textContent = articleObj.title;
+  article.append(articleH2);
+
+  // creating paragraph elements and adding as children of article
+  const paraDate = document.createElement('p');
+  paraDate.classList.add('date');
+  paraDate.textContent = articleObj.date;
+ 
+  const paragraph1 = document.createElement('p');
+  paragraph1.textContent = articleObj.firstParagraph;
+  
+  const paragraph2 = document.createElement('p');
+  paragraph2.textContent = articleObj.secondParagraph;
+  
+  const paragraph3 = document.createElement('p');
+  paragraph3.textContent = articleObj.thirdParagraph;
+
+  article.append(paraDate, paragraph1, paragraph2, paragraph3);
+  
+  // creating span.expandButton and adding to article
+  const spanExpandButton = document.createElement('span');
+  spanExpandButton.classList.add('expandButton');
+  spanExpandButton.textContent = '+';
+  article.append(spanExpandButton);
+
+  // click event for span.expandButton
+  spanExpandButton.addEventListener('click', event => {
+    article.classList.toggle('article-open');
+  });
+
+  return article;
+}
+
+
+// adding articles for every object in data
+data.forEach(item => {
+  const articles = document.querySelector('.articles');
+  articles.append(articleMaker(item));
+})
